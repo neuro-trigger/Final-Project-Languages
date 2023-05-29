30fd5680d1620dab893713a21fd02095a649e03f
Carlos Rios <carlosandresrio33@hotmail.com>
2023-05-28 19:06:14 -0500

    ANTL4 working apparently well

--- src/main.js
+++ src/main.js
@@ -4,3 +4,16 @@ import tmdParser from './tmdParser.js';
 import tmdVisitor from './tmdVisitor.js';
 
 console.log("Hola Mundo");
+
+let inputCode = 'Machine prueba ; NAset = {} ; Aset = {q0} ; Init = q0 ; INalphabet = {"0", "1"} ; Malphabet = {} ;Behaviour = {(q0, "0") : (q0, "1", LEFT), (q0, "1") : (q0, "0", STAY)} ;';
+let chars = new antlr4.InputStream(inputCode);
+let lexer = new tmdLexer(chars);
+let tokens = new antlr4.CommonTokenStream(lexer);
+let parser = new tmdParser(tokens);
+let codeAnalyzer = new tmdVisitor();
+
+parser.buildParseTrees = true;
+
+let parseTree = parser.description();
+
+console.log(parseTree.toStringTree(parser.ruleNames));
